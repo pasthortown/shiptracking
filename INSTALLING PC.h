e2c6b1dbe32f2362c8aa56fa3c1bfeeb29c224a1
pasthortown <pastorchessmaster@gmail.com>
2018-04-24 09:35:52 -0500

    listo

--- INSTALLING PC.h
+++ INSTALLING PC.h
@@ -1,5 +1,5 @@
 sudo apt-get install apache2 mysql-client mysql-server php7.0 libapache2-mod-php7.0 graphviz aspell php7.0-pspell php7.0-curl php7.0-gd php7.0-intl php7.0-mysql php7.0-xml php7.0-xmlrpc php7.0-ldap php7.0-zip php7.0-imap
-sudo apt-get install git vlc filezilla brasero r-base rstudio libreoffice gimp godot inkscape wkhtmltopdf youtube-dl ffmpeg tesseract-ocr
+sudo apt-get install git vlc filezilla gimp wkhtmltopdf youtube-dl ffmpeg tesseract-ocr
 sudo apt-get install npm
 sudo npm install npm@latest -g
 sudo npm i -g npm
