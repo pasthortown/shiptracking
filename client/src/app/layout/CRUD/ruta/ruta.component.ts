6119c3ab31ecb5173c8d6ea9ea6f663923a89397
pasthortown <pastorchessmaster@gmail.com>
2018-04-24 09:34:56 -0500

    listo

--- client/src/app/layout/CRUD/ruta/ruta.component.ts
+++ client/src/app/layout/CRUD/ruta/ruta.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Component, OnInit, ViewContainerRef, ViewChild } from '@angular/core';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { Ruta } from '../../../entidades/CRUD/Ruta';
 import { RutaService } from './ruta.service';
 
+import { } from '@types/googlemaps';
 import 'rxjs/add/operator/toPromise';
 import { ModalComponent } from '../../bs-component/components';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
@@ -15,6 +16,8 @@ import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 })
 
 export class RutaComponent implements OnInit {
+   @ViewChild('gmap') gmapElement: any;
+   map: google.maps.Map;
 
    busy: Promise<any>;
    entidades: Ruta[];
@@ -212,6 +215,52 @@ export class RutaComponent implements OnInit {
       this.paginaActual=1;
       this.registrosPorPagina = 5;
       this.refresh();
+
+      const mapProp = {
+        center: new google.maps.LatLng(-0.224710, -78.516763),
+        zoom: 17,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    let location = new google.maps.LatLng(-0.224710, -78.516763);
+    var contentString = '<div id="content">'+
+  '<div id="siteNotice">'+
+  '</div>'+
+  '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+  '<div id="bodyContent">'+
+  '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+  'sandstone rock formation in the southern part of the '+
+  'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+  'south west of the nearest large town, Alice Springs; 450&#160;km '+
+  '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+  'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+  'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+  'Aboriginal people of the area. It has many springs, waterholes, '+
+  'rock caves and ancient paintings. Uluru is listed as a World '+
+  'Heritage Site.</p>'+
+  '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+  'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+  '(last visited June 22, 2009).</p>'+
+  '</div>'+
+  '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+    var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+    let marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      draggable: true,
+      icon: image,
+      title: 'YAVIRAC'
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(this.map, marker);
+      });
+
    }
 
    onSelect(entidadActual: Ruta): void {
