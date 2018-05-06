b6ab5d106b73fa3ffa553bd9da68c9735a9e6e0d
pasthortown <pastorchessmaster@gmail.com>
2018-05-06 12:10:51 -0500

    listo

--- client/src/app/login/login.component.spec.ts
+++ client/src/app/login/login.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +8,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
