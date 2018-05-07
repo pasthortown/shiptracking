05a7eb6dc272b75fc27d34bfcfcb04940be4781d
pasthortown <pastorchessmaster@gmail.com>
2018-05-07 18:36:42 -0500

    listo

--- gpstracking-expressions/src/pages/home/home.ts
+++ gpstracking-expressions/src/pages/home/home.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Persona } from './../register/Persona';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  usuario: string;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  ngOnInit(){
+    let contenido = JSON.parse(sessionStorage.getItem('logedResult')) as Persona;
+    this.usuario = contenido.nombres + ' ' + contenido.apellidos;
+  }
 }
