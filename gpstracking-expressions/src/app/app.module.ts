33e13167355309abfe663359761d0b47d210b768
pasthortown <pastorchessmaster@gmail.com>
2018-05-10 11:06:41 -0500

    about expressions

--- gpstracking-expressions/src/app/app.module.ts
+++ gpstracking-expressions/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
@@ -20,7 +19,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     LoginPage,
@@ -35,7 +33,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     LoginPage,
